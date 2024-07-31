cb762af524b707aa28a7c9bec916229a00c32af6
jaeyounging <5061yo@naver.com>
2024-07-31 14:35:31 +0900

    글로벌 css 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       {/* <body className={inter.className}>{children}</body> */}
       <body>{children}</body>
     </html>
