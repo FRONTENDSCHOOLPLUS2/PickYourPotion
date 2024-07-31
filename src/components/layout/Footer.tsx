5a494ed23618d43bc031e215624c9f0ffd744fd5
jaeyounging <5061yo@naver.com>
2024-07-31 16:55:54 +0900

    style: Footer 컴포넌트 생성

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+  return (
+    <footer className="py-3 px-6 text-darkGray description bg-whiteGray rounded-t-xl">
+      <h1 className="font-medium text-[10px]">멋쟁이 사자처럼 프론트엔드 스쿨 플러스</h1>
+      <div className="font-light text-[10px]">
+        <p>고객센터: 010 - 2944 - 3793</p>
+        <p>평일 09:30 - 18:30, 주말 휴무</p>
+        <p className="mt-3">대표 : 박재웅</p>
+        <p>
+          개발자 : <span className="font-medium">조정현, 박재웅, 송재웅, 이재영</span>
+        </p>
+        <p className="mt-3">통신판매 신고번호 : 1994 - 11 - 11</p>
+        <p>주소 : 서울 중구 퇴계로 324 10층 멋쟁이 사자처럼</p>
+        <p>정보보호 책임자 : 박재웅</p>
+        <p>대표 전화 : 010 - 2944 - 3793</p>
+        <p>이메일 : sanulim94@gmail.com</p>
+        <p className="mt-3 text-lightGray text-[9px]">
+          조지주는 통신판매중개자로서 통신판매 당사자가 아니며, 판매자가 등록한 상품정보 및 거래에
+          대해 조지주는 책임을 지지 않습니다. 고객님의 안전거래를 위해 현금 등으로 결제 시 저희
+          쇼핑몰에서 가입한 NICE구매안전 (에스크로) 서비스를 이용하실 수 있습니다.
+        </p>
+      </div>
+    </footer>
+  );
+}
