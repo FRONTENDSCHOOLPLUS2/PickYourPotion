9b9c9a3302a7630a0952d3e200dd5b60c943e1c2
donguramee <sanulim94@gmail.com>
2024-08-01 11:13:32 +0900

    🎨  navbar, header 첨포넌트 제작

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,32 @@
 import Link from "next/link";
-// import { useState } from "react";
-// import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Navbar() {
-  // const router = useRouter();
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const handleClick = (index: number) => {
+    setActiveIndex(index);
+  };
 
   const links = [
     { href: "/main", label: "오늘의 술" },
-    { href: "/market", label: "술 창고" },
-    { href: "/community", label: "술상" },
-    { href: "/map", label: "대동술지도" },
+    { href: "/market", label: "술상" },
+    { href: "/community", label: "술창고" },
+    { href: "/map", label: "지도" },
   ];
   return (
     <ul className="flex flex-row h-12 items-center mx-5 justify-between">
-      {links.map((link.index))<li></li>}
+      {links.map((link, index) => (
+        <li key={index} onClick={() => handleClick(index)} className="list-none">
+          <Link href={link.href}>
+            <span
+              className={`cursor-pointer ${activeIndex === index ? "underline decoration-4 underline-offset-8" : "text-[gray]"} subTitleMedium`}
+            >
+              {link.label}
+            </span>
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 }
-
-// <Link href="/main" className="cursor-point subTitleMedium text-gray text-[20px]">
-//   오늘의 술
-// </Link>
-// <Link href="/market" className="cursor-point subTitleMedium text-gray text-[20px]">
-//   술상
-// </Link>
-// <Link href="/community" className="cursor-point subTitleMedium text-gray text-[20px]">
-//   술창고
-// </Link>
-// <Link href="/map" className="cursor-point subTitleMedium text-gray text-[20px]">
-//   오늘의 술
-// </Link>
