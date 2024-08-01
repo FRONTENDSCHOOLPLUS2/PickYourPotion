afe4d0a60f38ba6b335ae311a25220a1c8ea7bb4
donguramee <sanulim94@gmail.com>
2024-08-01 18:36:23 +0900

    🎨  verticalCard component 완성

--- src/app/market/liquor/page.tsx
+++ src/app/market/liquor/page.tsx
@@ -1,5 +1,7 @@
+import VerticalCard from "@/components/VerticalCard";
+
 export default function Page() {
   return (
-    <div>page</div>
+    <VerticalCard/>
   )
 }
--- src/components/VerticalCard.tsx
+++ src/components/VerticalCard.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+import dummy from "../../public/community-dummy.png";
+
+
 export default function VerticalCard() {
-  return <div>VerticalCard</div>;
+
+
+  return (<div className="h-[284px] w-[182px] flex flex-col ">
+    <Image src={dummy} alt="술이미지" width={182} height={180} className="round mb-3"/>
+<span className="contentMedium text-[black]">나루 생 막걸리 11.5%</span>
+<span className="description text-[gray]">한강주조</span>
+<span className="description text-[gray]">깊고 묵직한 단맛의 막걸리</span>
+<span className="contentMedium text-[black]">32,000원</span>
+  </div>);
 }
