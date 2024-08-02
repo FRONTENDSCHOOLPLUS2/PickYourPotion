3cd2948b44c121e43a0be093d424470346641cbc
hardy-is-cat <whwjdguszz@naver.com>
2024-08-02 10:36:58 +0900

    [style] 헤더 아이콘 크기 변경, layout 메타데이터 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,19 +7,17 @@ import gnb from "../../../public/images/icons/icon-gnb.svg";
 
 export default function Header() {
   return (
-    <>
-      <div className="flex flex-row items-center justify-between h-12 mx-2">
-        <Image className="" src={gnb} alt="검색 아이콘" width={30} height={30} />
-        <span className="ml-8 font-medium text-[20px] text-center">지금 페이지</span>
-        <div className="flex flex-row">
-          <Link href="/search">
-            <Image className="" src={search} alt="검색 아이콘" width={30} height={30} />
-          </Link>
-          <Link href="/cart">
-            <Image className="" src={cart} alt="장바구니 아이콘" width={30} height={30} />
-          </Link>
-        </div>
+    <div className="flex flex-row items-center justify-between h-12 mx-2">
+      <Image className="" src={gnb} alt="검색 아이콘" width={40} height={40} />
+      <span className="ml-8 font-medium text-[20px] text-center">지금 페이지</span>
+      <div className="flex flex-row">
+        <Link href="/search">
+          <Image className="" src={search} alt="검색 아이콘" width={40} height={40} />
+        </Link>
+        <Link href="/cart">
+          <Image className="" src={cart} alt="장바구니 아이콘" width={40} height={30} />
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
