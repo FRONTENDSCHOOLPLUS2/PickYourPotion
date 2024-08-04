e705f79b90c3705f2c9d9335a68e68ff357baa6d
AYFG <sek82468246@gmail.com>
2024-08-05 00:07:05 +0900

    상품의 주종 도수 용량 소비기한 box 내부 요소 조정,장바구니,구매 버튼 sticky 조정,상품 좋아요 dummy 생성,후기 요소 정렬

--- src/app/detail/[id]/detail.tsx
+++ src/app/detail/[id]/detail.tsx
@@ -6,21 +6,6 @@ export default function Detail() {
       <div className="my-5">
         <Image src={detailDummy} width={428} height={450} alt="막걸리 이미지" />
       </div>
-      <div>
-        <div className="fixed bottom-0 flex flex-row gap-3 py-6 mt-5 mb-5 bg-white left-6 round">
-          <button
-            className={`contentMedium w-[124px] h-[62px] flex items-center justify-center cursor-pointe bg-whiteGray text-darkGray round`}
-          >
-            술바구니
-            <br /> 추가
-          </button>
-          <button
-            className={`contentMedium w-[244px] h-[62px] flex items-center  justify-center cursor-pointer bg-primary text-white round `}
-          >
-            구매하기
-          </button>
-        </div>
-      </div>
     </>
   );
 }
--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -5,60 +5,75 @@ import dummyImage from "../../../../public/community-dummy.png";
 import detailDummy from "../../../../public/detail_dummy.png";
 import smallDummyImage from "../../../../public/community-dummy-small.png";
 import iconLike from "../../../../public/images/icons/icon-like.svg";
-
+import iconLikeTrue from "../../../../public/images/icons/icon-like-true.svg";
 import { useState } from "react";
 import Detail from "./detail";
 import Reply from "./reply";
-export default function page() {
-  const [showDetail, setShowDetail] = useState(true);
 
+export default function Page() {
+  let content;
+  let likeBtn;
+  const [showDetail, setShowDetail] = useState(true);
+  const [like, setLike] = useState(false);
   const handleToggle = () => {
     setShowDetail(!showDetail);
   };
-  const handlLike = () => {
-    setShowDetail(!showDetail);
+  const handleLike = () => {
+    setLike(!like);
   };
-  let content;
+
   if (showDetail) {
     content = <Detail />;
   } else {
     content = <Reply />;
+  }
+
+  if (like) {
+    likeBtn = iconLikeTrue;
+  } else {
+    likeBtn = iconLike;
   }
   return (
     <div className="flex flex-col ">
       <div className="justify-center max-w-3xl">
         <Image src={dummyImage} width={428} height={450} alt="막걸리 이미지" />
       </div>
-      <div className="relative px-10 py-8 mt-[-30px]  max-w-[428px] bg-white topRound topShadow">
-        <h1 className="title">복순도가 소주</h1>
+      <div className="relative px-10 py-8 mt-[-35px]  max-w-[428px] bg-white topRound topShadow">
+        <div className="flex flex-row justify-between">
+          <h1 className="title flex items-center">복순도가 소주</h1>
+          <button onClick={handleLike}>
+            <Image src={likeBtn} width={32} height={22} alt="막걸리 이미지" />
+            <p className="flex description justify-center text-darkGray">4472</p>
+          </button>
+        </div>
         <p className="content text-darkGray text-ellipsis mt-2.5">
           Lorem Ipsum is simply dummy text of the printing and
         </p>
-        <p className="content text-darkGray text-ellipsis mt-2.5">양조장 이름</p>
-        <div className="flex flex-row mt-3">
-          <div className="w-[82px] h-[64px] flex flex-col items-center justify-center bg-ivory mr-4 round ">
-            <span className="text-black contentMedium">주종</span>
-            <p className="description text-center">
-              증류식 <br />
-            </p>
-            <p className="description m-auto">소주</p>
+        <p className="content text-darkGray text-ellipsis mt-2.5">복순도가 양조장</p>
+
+        <div className="flex flex-row mt-3 gap-4 ">
+          <div className="w-[82px] h-[64px] flex flex-col items-center justify-center bg-ivory round">
+            <span className="text-black contentMedium mt-2">주종</span>
+            <p className="description text-gray mt-[-2px]">증류식</p>
+            <p className="description text-gray mt-[-6px]">소주</p>
           </div>
-          <div className="w-[82px] h-[64px] flex flex-col items-center justify-center bg-ivory mr-4 round ">
-            <span className="text-black contentMedium">도수</span>
-            <p className="description">10도</p>
+
+          <div className="w-[82px] h-[64px] flex flex-col items-center justify-center bg-ivory round gap-1">
+            <span className="text-black contentMedium ">도수</span>
+            <p className="description text-gray">10도</p>
           </div>
-          <div className="w-[82px] h-[64px] flex flex-col items-center justify-center bg-ivory mr-4 round ">
+          <div className="w-[82px] h-[64px] flex flex-col items-center justify-center bg-ivory round ">
             <span className="text-black contentMedium">용량</span>
-            <p className="description">1L</p>
+            <p className="description text-gray">1L</p>
           </div>
-          <div className="w-[82px] h-[64px] flex flex-col items-center justify-center bg-ivory mr-4 round ">
+          <div className="w-[82px] h-[64px] flex flex-col items-center justify-center bg-ivory round ">
             <span className="text-black contentMedium">소비기한</span>
-            <p className="description">무제한</p>
+            <p className="description text-gray">무제한</p>
           </div>
         </div>
-        <div className="flex flex-row mt-5 border-2 round border-whiteGray bg-whiteGray ">
+        <div className="flex flex-row mt-5 round top-shadow bg-whiteGray ">
           <button
-            className={`contentMedium w-[186px] h-[52px] flex items-center justify-center cursor-pointer transition-colors round ${
+            className={`contentMedium w-[186px] h-[52px] flex items-center justify-center cursor-pointer  transition-colors round ${
               showDetail ? "bg-primary text-white " : "bg-whiteGray text-black"
             }`}
             onClick={() => setShowDetail(true)}
@@ -75,7 +90,7 @@ export default function page() {
           </button>
         </div>
         {content}
-        <div className="fixed bottom-0 flex flex-row gap-3 py-6 mt-5 mb-5 bg-white left-6 round">
+        <div className="sticky bottom-0 flex flex-row gap-3  mt-5 mb-5 bg-white left-6 round">
           <button
             className={`contentMedium w-[124px] h-[62px] flex items-center justify-center cursor-pointe bg-whiteGray text-darkGray round`}
           >
