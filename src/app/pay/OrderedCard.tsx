ad5f1fa4e9852f6215a53a7e72c3559d26d0ae5e
jaeyounging <5061yo@naver.com>
2024-08-05 18:26:16 +0900

    style: 결제 페이지 스타일 및 마크업

---
+++ src/app/pay/OrderedCard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+import OrderImage from "@/../public/community-dummy-small.png";
+
+const OrderedCard: React.FC = () => {
+  return (
+    <div className="flex justify-between border border-gray rounded-xl px-2 py-4">
+      <div className="flex">
+        <Image src={OrderImage} alt="" />
+        <div className="description text-gray ml-6">
+          <h2 className="contentBold text-black">로렘입숨 생막걸리</h2>
+          <p>17도</p>
+          <p>입생로랑 양조장</p>
+        </div>
+      </div>
+      <div className="flex items-end">
+        <p className="text-black text-[14px]">6500원/1개</p>
+      </div>
+    </div>
+  );
+};
+
+export default OrderedCard;
