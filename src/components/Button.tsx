cc916dece3da6340d41abe0de998266fc0781955
jaeyounging <5061yo@naver.com>
2024-08-05 13:48:37 +0900

    공통 버튼 컴포넌트 생성

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,21 @@
-export default function Button() {
-  return (
-    <div>Button</div>
-  )
+interface ButtonProps {
+  children: string;
+  onClick: () => {};
+  className?: string; // 추가적인 css
+  selected: boolean;
 }
+
+const Button: React.FC<ButtonProps> = ({ children, onClick, className, selected = false }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 rounded-lg focus:outline-none duration-300
+        ${selected ? "bg-primary text-black" : "bg-whiteGray text-darkGray"} 
+        ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
