0dcb751fbf46ec5babcb9bf1d21b39bf415b2c8a
donguramee <sanulim94@gmail.com>
2024-08-08 14:10:11 +0900

    🐛 메인페이지 슬라이더 수정, 네브바 수정, 버튼 수정

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -4,7 +4,7 @@ function CartButton() {
   return (
     <button
       type="button"
-      className="absolute bottom-3 flex justify-center items-center right-3 w-12 h-12 bg-primary rounded-full overflow-hidden cursor-pointer"
+      className="relative bottom-3 flex justify-center items-center right-3 w-12 h-12 bg-primary rounded-full overflow-hidden cursor-pointer"
     >
       <CartIcon className="fill-white" />
       <span className="a11y-hidden">술바구니에 담기</span>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,23 +1,34 @@
 import Image from "next/image";
-import CartButton from "./CartButton";
+import cart from "../../public/images/icons/icon-cart.svg";
 import dummy from "../../public/community-dummy.png";
 
 function ProductCard() {
   return (
-    <div className="flex flex-col w-[250px] h-[458px] bg-white rounded-[10px]">
-      <div className="h-[343px] rounded-[8px] overflow-hidden">
-        <div className="inset-0">
-          <div className="w-full h-full p-[10px]">
-            <Image src={dummy} alt="복순도가 막걸리 썸네일" height={343} width={230} />
-          </div>
-        </div>
+    <div className="flex flex-col w-[230px] bg-white round relative shadow-lg">
+      <div className="h-[324px] p-[10px] rounded-[10px]">
+        <Image
+          src={dummy}
+          alt="추천 술 이미지"
+          className="w-[230px] h-full object-cover rounded-[8px]"
+        />
       </div>
-      <div className="px-5 py-4 rounded-b-lg">
-        <h3 className="text-black contentMedium mb-1">복순도가 손 막걸리 X 3병</h3>
+      <div className="w-[230px] px-5 py-4 rounded-b-lg">
+        <h3 className="text-black subTitleMedium text-[18px] mb-1">복순도가 손 막걸리</h3>
         <p className="text-gray content mb-1">복순도가</p>
         <p className="text-black contentMedium mb-1">32,000원</p>
-        <CartButton />
       </div>
+      <button
+        type="button"
+        className="flex w-[46px] h-[46px] bg-primary rounded-full justify-center items-center absolute bottom-[20px] right-[10px] "
+      >
+        <Image
+          src={cart}
+          alt="장바구니 아이콘"
+          width={32}
+          height={32}
+          className="filter invert brightness-0 saturate-100"
+        />
+      </button>
     </div>
   );
 }
