ab988c7852c9f81cf39cff2ab5434b0b4b82e447
AYFG <88381607+AYFG@users.noreply.github.com>
2024-08-09 16:54:27 +0900

    Delete src/app/detail/[id]/reply.tsx

--- src/app/detail/[id]/reply.tsx
+++
@@ -1,7 +0,0 @@
-import { ProductDetail } from "./page";
-import ReplyItem from "./ReplyItem";
-
-export default function Reply({ data }: { data: ProductDetail }) {
-  const list = data.replies?.map((item) => <ReplyItem key={item._id} item={item} />);
-  return <>{list}</>;
-}
