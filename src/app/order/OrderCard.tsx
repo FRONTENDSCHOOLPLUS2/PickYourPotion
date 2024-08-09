309a54be56d455b41b39dade253aa5185602d0d9
AYFG <sek3137@naver.com>
2024-08-09 15:12:11 +0900

    대소문자 구분

---
+++ src/app/order/OrderCard.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import dummyImg from "@/../public/community-dummy-small.png";
+
+export default function OrderCard() {
+  return (
+    <div className="border border-gray rounded-xl py-3 px-3 flex justify-between my-2.5">
+      <div className="flex">
+        <Image src={dummyImg} alt="테스트 이미지" width={50} height={50} />
+        <div className="flex flex-col ml-6 text-xs text-gray">
+          <p className="text-base font-medium text-black">로렘입숨 생막걸리</p>
+          <span>17도</span>
+          <span>입샘로랑 양조장</span>
+        </div>
+      </div>
+      <div className="flex text-[13px] items-end text-black">
+        <span>24,000원/2개</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/order/OrderDate.tsx
@@ -0,0 +1,8 @@
+export default function OrderDate() {
+  return (
+    <div className="flex justify-between mt-8 mb-2 text-xs text-gray">
+      <span>2024.07.30 주문내역</span>
+      <span>{"자세히보기 >>"}</span>
+    </div>
+  );
+}
