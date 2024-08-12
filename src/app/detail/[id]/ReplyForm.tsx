4b60c793174d6bda34e47aae949e7358fe2b6029
AYFG <sek3137@naver.com>
2024-08-12 17:56:11 +0900

    상품 상세 불러오기, 타입 지정

---
+++ src/app/detail/[id]/ReplyForm.tsx
@@ -0,0 +1,8 @@
+export default function ReplyForm() {
+  return (
+    <form className="border-[0.5px] border-gray">
+      <input className="" placeholder="이번 술은 어떠셨나요?" />
+      <button type="submit">전송</button>
+    </form>
+  );
+}
