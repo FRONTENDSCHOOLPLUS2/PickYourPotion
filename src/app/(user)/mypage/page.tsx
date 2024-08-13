44913f59178b6eaf2b43513b2a1bdf4d736cd3ba
jaeyounging <5061yo@naver.com>
2024-08-13 17:20:27 +0900

    ✨ feature: 마이페이지에서 로그인 한 회원 정보 불러오기

--- src/app/(user)/mypage/page.tsx
+++ src/app/(user)/mypage/page.tsx
@@ -1,14 +1,48 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
 import MyPageButton from "./MyPageButton";
 import MyPageList from "./MyPageList";
+import { useEffect, useState } from "react";
 
 export default function MyPage() {
+  const API_SERVER = process.env.NEXT_PUBLIC_API_SERVER;
+  const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const url = `${API_SERVER}/users/4/name`;
+  const token = process.env.NEXT_PUBLIC_ACCESS_TOCKEN;
+
+  const [name, setName] = useState<string>("");
+
+  useEffect(() => {
+    const fetchUserName = async () => {
+      try {
+        const response = await fetch(url, {
+          headers: {
+            "client-id": `${CLIENT_ID}`,
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const result = await response.json();
+
+        if (response.ok) {
+          setName(result.item.name);
+        } else {
+          console.log(result.message);
+        }
+      } catch (error) {
+        console.error("네트워크 오류 발생", error);
+      }
+    };
+
+    fetchUserName();
+  }, []);
+
   return (
     <>
       <Navbar />
       <main className="px-10">
         <div className="px-3">
-          <h1 className="mt-[114px] text-2xl font-medium">조지주님</h1>
+          <h1 className="mt-[114px] text-2xl font-medium">{`${name}님`}</h1>
           <p className="text-2xl font-medium">안녕하세요👋🏻</p>
         </div>
         <MyPageButton />
