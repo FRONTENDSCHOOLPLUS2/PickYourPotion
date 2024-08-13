d68d74d2efd082e971b85419c3f713af930a41c2
hardy-is-cat <whwjdguszz@naver.com>
2024-08-13 14:56:26 +0900

    [Refactor] 다시 보지 않기 구현 #87

--- src/app/landing/setCookie.ts
+++ src/app/landing/setCookie.ts
@@ -6,9 +6,12 @@ import { redirect } from "next/navigation";
 // await이 필요없는 async 함수때문에 eslint 비활성화
 // eslint-disable-next-line
 export default async function setCookie() {
-  const oneDay = 60 * 60 * 24;
-  cookies().set("isLandingClose", "true", {
-    maxAge: oneDay * 7,
-  });
+  // 일주일간 보지 않기
+  // const oneDay = 60 * 60 * 24;
+  // cookies().set("isLandingClose", "true", {
+  //   maxAge: oneDay * 7,
+  // });
+  // 다시 보지 않기
+  cookies().set("isLandingClose", "true");
   redirect("/");
 }
