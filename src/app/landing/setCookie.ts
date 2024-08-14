093113bb465852bb331635edb05d62ec30426fa8
hardy-is-cat <whwjdguszz@naver.com>
2024-08-14 16:20:24 +0900

    [Bug] 검사 후 쇼핑몰이동 버튼 버그 수정 #104

--- src/app/landing/setCookie.ts
+++ src/app/landing/setCookie.ts
@@ -5,13 +5,18 @@ import { redirect } from "next/navigation";
 
 // await이 필요없는 async 함수때문에 eslint 비활성화
 // eslint-disable-next-line
-export default async function setCookie() {
-  // 일주일간 보지 않기
-  // const oneDay = 60 * 60 * 24;
-  // cookies().set("isLandingClose", "true", {
-  //   maxAge: oneDay * 7,
-  // });
+export async function setCookieCloseLanding() {
   // 다시 보지 않기
   cookies().set("isLandingClose", "true");
+  redirect("/");
+}
+
+// await이 필요없는 async 함수때문에 eslint 비활성화
+// eslint-disable-next-line
+export async function setCookieCloseLandingOneDay() {
+  // 하루동안 보지 않기
+  cookies().set("isLandingClose", "true", {
+    maxAge: 60 * 60 * 24,
+  });
   redirect("/");
 }
