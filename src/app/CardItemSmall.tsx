8852d49c14d3478255dc4b527de026b351aa9fc0
hardy-is-cat <whwjdguszz@naver.com>
2024-08-19 18:07:23 +0900

    [Feat] 메인페이지 상품, 리뷰섹션 생성 #111

--- src/app/CardItemSmall.tsx
+++ src/app/CardItemSmall.tsx
@@ -1,22 +1,26 @@
 import Image from "next/image";
+import { ProductDetail } from "./detail/[id]/page";
+import Link from "next/link";
 
-function CardItemSmall() {
+function CardItemSmall({ data }: { data: ProductDetail }) {
+  const API_SERVER = process.env.NEXT_PUBLIC_API_SERVER;
+
   return (
-    <div className="flex items-center">
+    <Link href={`/detail/${data._id}`} className="flex items-center">
       <Image
-        src="/images/product/naru-115.jpg"
-        alt="나루 생 막걸리 11.5%"
+        src={API_SERVER + data.mainImages[0].path}
+        alt={data.name}
         className="rounded-lg"
         width={120}
         height={120}
       />
       <div className="ml-5">
-        <h3 className="contentMedium text-black mb-1">나루 생 막걸리 11.5%</h3>
-        <p className="description text-gray mb-1">한강주조</p>
-        <p className="description text-gray mb-1">깊고 묵직한 단맛의 막걸리</p>
-        <p className="contentMedium text-black">32,000원</p>
+        <h3 className="mb-1 text-black contentMedium">{data.name}</h3>
+        <p className="mb-1 description text-gray">{data.extra.brewery}</p>
+        <p className="mb-1 description text-gray">깊고 묵직한 단맛의 막걸리</p>
+        <p className="text-black contentMedium">{data.price.toLocaleString()}원</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import CardItemSmall from "./CardItemSmall";
 import CardSwiper from "./CardSwiper";
 import Navbar from "@/components/Navbar";
 import { ProductDetail } from "./detail/[id]/page";
+import ReviewCardItem from "@/components/ReviewCardItem";
 
 async function fetchProductList(params?: string[][]): Promise<ProductDetail[]> {
   const API_SERVER = process.env.NEXT_PUBLIC_API_SERVER;
@@ -23,31 +24,116 @@ async function fetchProductList(params?: string[][]): Promise<ProductDetail[]> {
 
 export default async function Home() {
   const bestProduct = await fetchProductList([["custom", '{ "extra.isBest": true }']]);
+  const takjuProduct = await fetchProductList([
+    ["custom", '{ "extra.category": "PC01" }'],
+    ["limit", "3"],
+  ]);
+  const yakjuProduct = await fetchProductList([
+    ["custom", '{ "extra.category": "PC02" }'],
+    ["limit", "3"],
+  ]);
+  const spiritProduct = await fetchProductList([
+    ["custom", '{ "extra.category": "PC03" }'],
+    ["limit", "3"],
+  ]);
+  const wineProduct = await fetchProductList([
+    ["custom", '{ "extra.category": "PC04" }'],
+    ["limit", "3"],
+  ]);
+
   return (
     <>
       <Navbar />
       <main className="px-0">
         <CardSwiper />
-        <section id="product-best" className="px-[25px]">
-          <h2 className="mb-6 text-black subTitleMedium mt-5">베스트 상품</h2>
-          <ul className="mb-3 [&>*:last-child]:mb-0 ">
-            <li className="mb-3">
-              <Link href="/">
-                <CardItemSmall />
-              </Link>
-            </li>
-            <li className="mb-3">
-              <Link href="/">
-                <CardItemSmall />
-              </Link>
-            </li>
-            <li className="mb-3">
-              <Link href="/">
-                <CardItemSmall />
-              </Link>
-            </li>
-          </ul>
-        </section>
+        <div className="px-[25px]">
+          <section id="product-best" className="mb-12">
+            <h2 className="mt-5 mb-6 text-black subTitleMedium">베스트 상품</h2>
+            <ul className="mb-3 [&>*:last-child]:mb-0 ">
+              {bestProduct &&
+                bestProduct.map((item) => {
+                  return (
+                    <li className="mb-3" key={item._id}>
+                      <CardItemSmall data={item} />
+                    </li>
+                  );
+                })}
+            </ul>
+          </section>
+          <section id="product-takju" className="relative mb-12">
+            <h2 className="mt-5 mb-6 text-black subTitleMedium">탁주</h2>
+            <ul className="mb-3 [&>*:last-child]:mb-0 ">
+              {takjuProduct &&
+                takjuProduct.map((item) => {
+                  return (
+                    <li className="mb-3" key={item._id}>
+                      <CardItemSmall data={item} />
+                    </li>
+                  );
+                })}
+            </ul>
+            <Link href={"/market/raw"} className="absolute right-0 top-1 description text-gray">
+              더보기 &gt;
+            </Link>
+          </section>
+          <section id="product-yakju" className="relative mb-12">
+            <h2 className="mt-5 mb-6 text-black subTitleMedium">약・청주</h2>
+            <ul className="mb-3 [&>*:last-child]:mb-0 ">
+              {yakjuProduct &&
+                yakjuProduct.map((item) => {
+                  return (
+                    <li className="mb-3" key={item._id}>
+                      <CardItemSmall data={item} />
+                    </li>
+                  );
+                })}
+            </ul>
+            <Link href={"/market/refined"} className="absolute right-0 top-1 description text-gray">
+              더보기 &gt;
+            </Link>
+          </section>
+          <section id="product-spirit" className="relative mb-12">
+            <h2 className="mt-5 mb-6 text-black subTitleMedium">증류주</h2>
+            <ul className="mb-3 [&>*:last-child]:mb-0 ">
+              {spiritProduct &&
+                spiritProduct.map((item) => {
+                  return (
+                    <li className="mb-3" key={item._id}>
+                      <CardItemSmall data={item} />
+                    </li>
+                  );
+                })}
+            </ul>
+            <Link href={"/market/liquor"} className="absolute right-0 top-1 description text-gray">
+              더보기 &gt;
+            </Link>
+          </section>
+          <section id="product-wine" className="relative mb-12">
+            <h2 className="mt-5 mb-6 text-black subTitleMedium">과실주</h2>
+            <ul className="mb-3 [&>*:last-child]:mb-0 ">
+              {wineProduct &&
+                wineProduct.map((item) => {
+                  return (
+                    <li className="mb-3" key={item._id}>
+                      <CardItemSmall data={item} />
+                    </li>
+                  );
+                })}
+            </ul>
+            <Link href={"/market/fruit"} className="absolute right-0 top-1 description text-gray">
+              더보기 &gt;
+            </Link>
+          </section>
+          <section id="review-list" className="mb-12">
+            <h2 className="mt-5 mb-6 text-black subTitleMedium">실시간 리뷰</h2>
+            <div className="flex flex-wrap justify-between gap-y-3 review-wrapper">
+              <ReviewCardItem />
+              <ReviewCardItem />
+              <ReviewCardItem />
+              <ReviewCardItem />
+            </div>
+          </section>
+        </div>
       </main>
     </>
   );
