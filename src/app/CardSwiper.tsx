cefb2e2b0b81be20703cc22d6cb79b48bfcf96d7
hardy-is-cat <whwjdguszz@naver.com>
2024-08-19 23:55:36 +0900

    [Refactor] 메인 최상단 swiper css 수정 #111

--- src/app/CardSwiper.tsx
+++ src/app/CardSwiper.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import ProductCard from "@/components/ProductCard";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,58 +10,53 @@ function CardSwiper() {
 
   return (
     <div className="pt-20">
-      <div className="flex w-screen h-[530px] bg-ivory items-center relative overflow-hidden">
-        <div className="w-full justify-center items-center absolute">
-          <Swiper
-            slidesPerView={1.7}
-            spaceBetween={10}
-            // centeredSlides={true}
-            slidesOffsetBefore={20}
-            slidesOffsetAfter={20}
-            onProgress={(swiper, progress) => {
-              if (progress > 0.9) {
-                setHideElement(true);
-              } else {
-                setHideElement(false);
-              }
-            }}
-            breakpoints={{
-              640: {
-                slidesPerView: 1.7,
-                spaceBetween: 20,
-              },
-              768: {
-                slidesPerView: 3.3,
-                spaceBetween: 30,
-              },
-              1024: {
-                slidesPerView: 4.5,
-                spaceBetween: 40,
-              },
-            }}
-          >
-            <SwiperSlide className="ml-[25px]">
-              <ProductCard />
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard />
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard />
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard />
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard />
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard />
-            </SwiperSlide>
-          </Swiper>
-        </div>
+      <div className="relative w-screen py-10 overflow-hidden bg-ivory">
+        <Swiper
+          id="main-is-new-swiper"
+          slidesPerView={1.3}
+          spaceBetween={10}
+          onProgress={(swiper, progress) => {
+            if (progress > 0.9) {
+              setHideElement(true);
+            } else {
+              setHideElement(false);
+            }
+          }}
+          breakpoints={{
+            360: {
+              slidesPerView: 1.5,
+            },
+            640: {
+              slidesPerView: 2.5,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+        </Swiper>
         {!hideElement && (
-          <div className="flex w-[100px] h-full absolute z-10 right-0 bg-custom-white pointer-events-none"></div>
+          <div className="absolute top-0 right-0 z-10 flex w-1/5 h-full pointer-events-none bg-custom-white"></div>
         )}
       </div>
     </div>
