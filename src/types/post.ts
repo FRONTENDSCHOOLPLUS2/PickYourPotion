59b686ec4756f9ac6c9ab2a68d73b88f392dfc00
AYFG <sek3137@naver.com>
2024-08-20 18:22:30 +0900

    develop push 3

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,26 +1,24 @@
-import { User } from "./user"
+import { UserData } from "./user";
 
 export interface PostComment {
-  _id: number,
-  user_id: number,
-  user: User,
-  content: string,
-  like: number,
-  createdAt: string,
-  updatedAt: string,
+  _id: number;
+  user_id: number;
+  user: UserData;
+  content: string;
+  like: number;
+  createdAt: string;
+  updatedAt: string;
 }
 
 export interface Post {
-  _id: number,
-  type?: string,
-  title: string,
-  content: string,
-  user: Pick<User, '_id' | 'name' | 'profileImage'>,
-  views: number,
-  repliesCount: number,
-  replies?: PostComment[],
-  createdAt: string,
-  updatedAt: string,
+  _id: number;
+  type?: string;
+  title: string;
+  content: string;
+  user: Pick<UserData, "_id" | "name" | "profileImage">;
+  views: number;
+  repliesCount: number;
+  replies?: PostComment[];
+  createdAt: string;
+  updatedAt: string;
 }
-
-
