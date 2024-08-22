f6ab16763c5e6ef0e9cd7d48f0b0c1a02e6008bb
AYFG <sek3137@naver.com>
2024-08-22 18:04:55 +0900

    전역 상태 퍼시스트로 세션에 저장

--- src/zustand/Store.ts
+++ src/zustand/Store.ts
@@ -40,20 +40,42 @@ interface ProductStore {
   setQuantity: (quantity: number) => void;
   brewery: string;
   setBrewery: (brewery: string) => void;
+  alcohol: string;
+  setAlcohol: (alcohol: string) => void;
 }
-export const useProductStore = create<ProductStore>((set) => ({
-  showDetail: true,
-  setShowDetail: (showDetail: boolean) => set({ showDetail }),
-  _id: 0,
-  setId: (_id: number) => set({ _id }),
-  name: "",
-  setName: (name: string) => set({ name }),
-  price: 0,
-  setPrice: (price: number) => set({ price }),
-  image: "",
-  setImage: (image: string) => set({ image }),
-  quantity: 0,
-  setQuantity: (quantity: number) => set({ quantity }),
-  brewery: "",
-  setBrewery: (brewery: string) => set({ brewery }),
-}));
+export const useProductStore = create<ProductStore>()(
+  persist(
+    (set) => ({
+      showDetail: true,
+      setShowDetail: (showDetail: boolean) => set({ showDetail }),
+      _id: 0,
+      setId: (_id: number) => set({ _id }),
+      name: "",
+      setName: (name: string) => set({ name }),
+      price: 0,
+      setPrice: (price: number) => set({ price }),
+      image: "",
+      setImage: (image: string) => set({ image }),
+      quantity: 1,
+      setQuantity: (quantity: number) => set({ quantity }),
+      Alcohol: "",
+      brewery: "",
+      setBrewery: (brewery: string) => set({ brewery }),
+      alcohol: "",
+      setAlcohol: (alcohol: string) => set({ alcohol }),
+    }),
+    {
+      name: "product-storage",
+      getStorage: () => sessionStorage,
+      partialize: (state) => ({
+        _id: state._id,
+        name: state.name,
+        price: state.price,
+        image: state.image,
+        quantity: state.quantity,
+        brewery: state.brewery,
+        alcohol: state.alcohol,
+      }),
+    },
+  ),
+);
