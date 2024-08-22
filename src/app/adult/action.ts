9e5b3009ee4c3578b6d255f37773a4e4cf7bb96d
hardy-is-cat <whwjdguszz@naver.com>
2024-08-23 01:52:42 +0900

    [Feat] 본인인증 후 유저 정보에 성인유무 저장 구현, 분기에 따른 모달 생성 #131

--- src/app/adult/action.ts
+++ src/app/adult/action.ts
@@ -1,11 +1,50 @@
 "use server";
 
+import { Router } from "next/router";
 import { RequestCertificationResponseCallback } from "./types";
+import { auth } from "@/auth";
+import { redirect, useRouter } from "next/navigation";
 
 const IMP_API_KEY = process.env.NEXT_PUBLIC_API_V1_REST_API_KEY;
 const IMP_API_SECRET = process.env.NEXT_PUBLIC_API_V1_REST_API_SECRET;
+const API_SERVER = process.env.NEXT_PUBLIC_API_SERVER;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
 
-// 서버로부터 access-token을 받아오는 함수
+// 유저의 id와 access-token을 가져오는 함수
+export const getUserIdToken = async () => {
+  const session = await auth();
+  const userId = session?.user?.id!;
+  const userAccessToken = session?.accessToken;
+  return { userId, userAccessToken };
+};
+
+// 서버에 저장된 유저 정보를 가져오는 함수
+export const getUserInfo = async (userId: string) => {
+  const res = await fetch(`${API_SERVER}/users/${userId}`, {
+    headers: {
+      "client-id": `${CLIENT_ID}`,
+    },
+  });
+  return res.json();
+};
+
+// 유저 정보의 extra값에 {isAdult: true}를 업데이트하는 함수
+const updateUserIsAdult = async (userId: string, extraInfo: object, accessToken: string) => {
+  const res = await fetch(`${API_SERVER}/users/${userId}`, {
+    method: "PATCH",
+    headers: {
+      "client-id": `${CLIENT_ID}`,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      extra: { ...extraInfo, isAdult: true },
+    }),
+  });
+  return res.json();
+};
+
+// 포트원 서버로부터 access-token을 받아오는 함수
 export const getToken = async () => {
   try {
     const data = await fetch("https://api.iamport.kr/users/getToken", {
@@ -24,12 +63,10 @@ export const getToken = async () => {
 
 // 본인인증 완료 후 실행될 콜백함수
 export const certificationCallback: RequestCertificationResponseCallback = async (response) => {
-  console.log(response);
   const { success, error_msg } = response;
 
   if (success) {
     const userImpUid = response.imp_uid;
-
     const tokenData = await getToken();
     const { access_token } = tokenData.response;
 
@@ -40,7 +77,21 @@ export const certificationCallback: RequestCertificationResponseCallback = async
     });
     const userInfoJsonData = await getCertifications.json();
 
-    console.log("사용자의 생년월일", userInfoJsonData.response.birthday);
+    // 고객의 생일 정보로부터 출생년 추출
+    const userBirthYear = +userInfoJsonData.response.birthday.slice(0, 4);
+
+    // 사용자가 성인일 경우 -> 서버에 저장된 고객정보에 isAdult 추가 후 상품페이지로 이동
+    if (new Date().getFullYear() - userBirthYear >= 19) {
+      const { userId, userAccessToken } = await getUserIdToken();
+      const userInfo = await getUserInfo(userId);
+      const userExtraInfo = userInfo.item.extra;
+      updateUserIsAdult(userId, userExtraInfo, userAccessToken!);
+      // stateless 모달 출력
+      redirect("adult/?confirmSuccess=true");
+    } else {
+      // 사용자가 성인이 아닐 경우 -> 성인만 구매할 수 있는 상품입니다 모달 출력
+      redirect("adult/?confirmFailed=true");
+    }
   } else {
     console.error(`본인인증 실패! : ${error_msg}`);
   }
--- src/app/adult/page.tsx
+++ src/app/adult/page.tsx
@@ -1,12 +1,50 @@
 "use client";
 
 import Button from "@/components/Button";
-import { certificationCallback } from "./action";
+import { certificationCallback, getUserInfo } from "./action";
 import { ExtendedIamport } from "./types";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const V1_IMP_KEY = process.env.NEXT_PUBLIC_API_V1_IMP_KEY;
-function Adult() {
+
+type SearchParamProps = {
+  searchParams: Record<string, string> | null | undefined;
+};
+
+function Adult({ searchParams }: SearchParamProps) {
+  // url의 request값을 받아와 변수에 저장
+  const request = searchParams?.request;
+  const confirmSuccess = searchParams?.confirmSuccess;
+  const confirmFailed = searchParams?.confirmFailed;
+
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const userId = session?.user?.id;
+
+  const isLoggedIn = async () => {
+    if (status === "authenticated") {
+      // 회원 정보 불러와서 성인인증 유무 확인
+      const userInfo = await getUserInfo(userId!);
+      if (userInfo.item.extra.isAdult) {
+        // 로그인이 되어있고 성인인증도 돼있을 때
+        alert("술 구매 가능!");
+        router.push("/pay");
+      } else {
+        // 로그인은 되어있지만 성인인증이 되지 않았을 때
+        // url의 request값이 true면 모달 띄우기
+        router.push("/adult?request=true");
+      }
+    } else {
+      // 로그인이 안 돼있을 때
+      alert("로그인 후 이용하실 수 있습니다.");
+    }
+  };
+
+  // 본인인증 창 호출 함수
   const onCertification = () => {
+    if (!session) return;
     if (!window.IMP) return;
 
     // 가맹점 식별하기
@@ -25,7 +63,33 @@ function Adult() {
   return (
     <>
       <h1>성인인증 테스트 페이지</h1>
+      <Button onClick={isLoggedIn}>구매하기</Button>
       <Button onClick={onCertification}>성인인증하기</Button>
+      {request && (
+        <>
+          <div className="bg-primary">
+            성인인증 해주십시오!!!
+            <Button onClick={onCertification}>성인인증하기</Button>
+            <Link href="/adult">모달 없애기</Link>
+          </div>
+        </>
+      )}
+      {confirmSuccess && (
+        <>
+          <div>
+            성인인증이 완료되었습니다. 상품을 구매해주세요.
+            <Link href="/adult">구매하러 가기</Link>
+          </div>
+        </>
+      )}
+      {confirmFailed && (
+        <>
+          <div>
+            성인부터 구매할 수 있는 상품입니다. 다음 기회에 봬요.
+            <Link href={"/"}>홈으로</Link>
+          </div>
+        </>
+      )}
     </>
   );
 }
