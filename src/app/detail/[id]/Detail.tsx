8b1c2b51f83ae9c2c24d0e93b343aed61b6f45ff
AYFG <sek82468246@gmail.com>
2024-08-25 16:40:11 +0900

    상품 상세 페이지 고정 너비 삭제, 구매 장바구니 버튼 밑 불투명 스타일 짙게 변경

--- src/app/detail/[id]/Detail.tsx
+++ src/app/detail/[id]/Detail.tsx
@@ -4,13 +4,14 @@ import Image from "next/image";
 export default function Detail({ data }: { data: ProductDetail }) {
   const contentImg = data.extra?.detailImage.map((item, index) => {
     return (
-      <div key={index} className="mt-3">
+      <div key={index} className="mt-3 flex justify-center">
         <Image
           src={`https://api.fesp.shop${item?.path}`}
           width={500}
           height={500}
-          alt="test"
+          alt="상세 설명"
           key={index}
+          className=" object-contain"
         />
       </div>
     );
