193866a6d6b9115c39c189e7605fad72be9ed496
AYFG <sek3137@naver.com>
2024-08-26 17:23:19 +0900

    상세 페이지 스타일 수정

--- src/app/detail/[id]/Detail.tsx
+++ src/app/detail/[id]/Detail.tsx
@@ -4,17 +4,18 @@ import Image from "next/image";
 export default function Detail({ data }: { data: ProductDetail }) {
   const contentImg = data.extra?.detailImage.map((item, index) => {
     return (
-      <div key={index} className="flex justify-center mt-3">
-        <Image
-          src={`https://api.fesp.shop${item?.path}`}
-          width="0"
-          height="0"
-          sizes="100vw"
-          className="w-full h-auto"
-          alt="상세 설명"
-          key={index}
-        />
-      </div>
+      <>
+        <div className="flex justify-center mt-3 " key={data._id}>
+          <Image
+            src={`https://api.fesp.shop${item?.path}`}
+            width="0"
+            height="0"
+            sizes="100vw"
+            className="w-full h-auto "
+            alt="상세 설명"
+          />
+        </div>
+      </>
     );
   });
 
