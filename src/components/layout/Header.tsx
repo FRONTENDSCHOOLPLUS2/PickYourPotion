d77617ce613747bc773ff66502519d6a3ef0f776
donguramee <sanulim94@gmail.com>
2024-08-27 19:55:20 +0900

    🐛 최종 배포

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 import cart from "../../../public/images/icons/icon-cart.svg";
 import search from "../../../public/images/icons/icon-search.svg";
-import gnb from "../../../public/images/icons/icon-gnb.svg";
+import logo from "../../../public/images/LOGO.png";
 
 export default function Header() {
   return (
     <>
       <div className="flex flex-row items-center justify-between h-14 fixed top-0 w-full px-2 z-20 bg-white shadow-sm">
-        <Link href="/">
-          <Image className="" src={gnb} alt="검색 아이콘" width={40} height={40} />
+        <Link href="/" className="ml-2">
+          <Image className="" src={logo} alt="검색 아이콘" width={40} height={40} />
         </Link>
         <div className="flex flex-row">
           <Link href="/search">
